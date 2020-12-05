cc5983ada41cfc5cbbe800af21672eabbde840f4
Viktoryia <vikylkinnelya@gmail.com>
2020-12-05 13:31:43 +0300

    add timer

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     tabsParent.addEventListener('click', (ev) => { //обертка со всеми табами делегир одинаковое событие от клика на все
         const target = ev.target;
-        if (target && target.classList.contains ('tabheader__item')) { //ищем таб
+        if (target && target.classList.contains('tabheader__item')) { //ищем таб
             tabs.forEach((el, idx) => { //перебрать все табы
                 if (target == el) { //сравнить табы с искомым
                     hideTabContent(); //скрыть старые табы
@@ -39,6 +39,71 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+
+    //функция устан таймер
+    //определтя разницу во времени, дедлайн
+    //вычислить время
+    //найти разницу
+    //обновление таймера
+
+    const deadline = '2020-12-6';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - new Date(),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds,
+        };
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`; //добавляется 0 если число однозначное
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector), //вводится при вызове функции
+            days = timer.querySelector('#days'), // # for searching айди
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock(); //для того, чтобы часы обновлялись сразу после обновления странцы без перескока
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime); //обьект со всеми временами 
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            function stopClock() {
+                if (t.total <= 0) { //если сегодняшняя дата больше дедлайна
+                    clearInterval(timeInterval); //интервал очищается дедлайн не считается
+                }
+            }
+        }
+    }
+    setClock('.timer', deadline);
+
+
+
+
+
+
+
 
 
 

1a84b44e9add90e859e534856c787dc9b44387e9
Viktoryia Nelia <75176786+vikylkinnelya@users.noreply.github.com>
2020-12-18 12:04:29 +0300

    add calculator

--- js/script.js
+++ js/script.js
@@ -370,10 +370,14 @@ window.addEventListener('DOMContentLoaded', () => {
         dotsList[slideIdx-1].style.opacity = 1; //горит активная
     }
 
+    function deleteNotDig(str) {
+        return +str.replace(/\D/g, '');
+    }
+
     dotsList.forEach( el => el.addEventListener('click', (ev) => {
         slideIdx = ev.target.dataset.idx;
-        offset = +width.replace(/\D/g, '');
-        offset = +width.replace(/\D/g, '') * (slideIdx-1);
+        offset = deleteNotDig(width);
+        offset = deleteNotDig(width) * (slideIdx-1);
         slidesField.style.transform = `translateX(-${offset}px)`; 
         
         currSliderNumber.innerText = getZero(slideIdx); //добавить 0 к текущему номеру
@@ -382,10 +386,10 @@ window.addEventListener('DOMContentLoaded', () => {
     }));
 
     nextBtn.addEventListener('click',() => {
-        if (offset == +width.replace(/\D/g, '') * (slides.length - 1)) { //если максимум, перейти в начало
+        if (offset == deleteNotDig(width) * (slides.length - 1)) { //если максимум, перейти в начало
             offset = 0;
         } else {
-            offset += +width.replace(/\D/g, ''); //отступ увеличивается на один слайд
+            offset += deleteNotDig(width); //отступ увеличивается на один слайд
         }
         slidesField.style.transform = `translateX(-${offset}px)`;
         
@@ -402,9 +406,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     prevBtn.addEventListener('click',() => {
         if (offset == 0) { //если минимум -- перейти в конец
-            offset = +width.replace(/\D/g, '') * (slides.length - 1);
+            offset = deleteNotDig(width) * (slides.length - 1);
         } else {
-            offset -= +width.replace(/\D/g, ''); //уменьшение отступа на один слайд
+            offset -= deleteNotDig(width); //уменьшение отступа на один слайд
         }
         slidesField.style.transform = `translateX(-${offset}px)`;
     
@@ -453,11 +457,67 @@ window.addEventListener('DOMContentLoaded', () => {
     }); */
 
     
-    
-    
-    
-    
-    
+    const result = document.querySelector('.calculating__result span');
+    let sex = 'female', //по умолчанию для работающей формы, где изнач чтото выбрано
+        height, weight, age, 
+        ratio = '1.375';
 
-    
+    function calcTotal () {
+        if (!sex || !height || !weight || !age || !ratio){ //если чтото не заполнено
+            result.textContent = ('_____');
+            return;
+        }
+        if (sex === 'female') {
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+        } else {
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+        }
+    }
+    calcTotal();
+
+    function getStaticInformation(parentSelector, activeClass) {
+        const elements = document.querySelectorAll(`${parentSelector} div`); //все дивы внутри родителя
+
+        elements.forEach( el => {
+            el.addEventListener('click', (ev) => {
+                if (ev.target.getAttribute('data-ratio')) { 
+                    ratio = +ev.target.getAttribute('data-ratio'); //рацион
+                } else {
+                    sex = ev.target.getAttribute('id'); //получаем пол
+                }
+                elements.forEach(el => el.classList.remove(activeClass)); //скрыть активн неактивных
+            
+                ev.target.classList.add(activeClass); //класс активности для выбранного
+                calcTotal();
+            });
+        });
+    }
+
+    getStaticInformation('#gender', 'calculating__choose-item_active'); //для пола
+    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active'); //для физ активности
+
+    function getDynamicInformation(selector) {
+        const input = document.querySelector(selector);
+
+        input.addEventListener('input', () => {
+            switch(input.getAttribute('id')) {
+                case 'height':
+                    height = +input.value;
+                    break;
+                case 'weight':
+                    weight = +input.value;
+                    break;
+                case 'age':
+                    age = +input.value;
+                    break;
+            }
+            calcTotal();
+        });
+    }
+
+    getDynamicInformation('#height');
+    getDynamicInformation('#weight');
+    getDynamicInformation('#age');
+
+
 });
